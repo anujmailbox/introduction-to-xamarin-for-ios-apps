7ebc0803bdec80618d39575f4861a62054b26454
Adam Brown <adamarthurryan@gmail.com>
2016-06-09 13:23:37 -0400

    initial project checkin

---
+++ finish/App5/ViewController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UIKit;
+
+namespace App5
+{
+    public enum Operator {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class Calculator {
+        public string Calculate( string operand1, string operand2, Operator @operator ) {
+            double value1;
+            double value2;
+
+            double.TryParse(operand1, out value1);
+            double.TryParse(operand2, out value2);
+
+            double result = 0;
+
+            switch (@operator)
+            {
+                case Operator.Addition:
+                    result = value1 + value2;
+                    break;
+                case Operator.Subtraction:
+                    result = value1 - value2;
+                    break;
+                case Operator.Multiplication:
+                    result = value1 * value2;
+                    break;
+                case Operator.Division:
+                    result = value1 / value2;
+                    break;
+            }
+
+            return result.ToString( );
+        }
+    }
+
+    public partial class ViewController : UIViewController {
+        private string _operand1 = string.Empty;
+        private string _operand2 = string.Empty;
+        private Operator _operator;
+
+        private Calculator _calculator;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            _calculator = new Calculator();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            txtDisplay.Text = string.Empty;
+
+            btnZero.TouchUpInside += NumericButtonClick;
+            btnOne.TouchUpInside += NumericButtonClick;
+            btnTwo.TouchUpInside += NumericButtonClick;
+            btnThree.TouchUpInside += NumericButtonClick;
+            btnFour.TouchUpInside += NumericButtonClick;
+            btnFive.TouchUpInside += NumericButtonClick;
+            btnSix.TouchUpInside += NumericButtonClick;
+            btnSeven.TouchUpInside += NumericButtonClick;
+            btnEight.TouchUpInside += NumericButtonClick;
+            btnNine.TouchUpInside += NumericButtonClick;
+
+            btnAdd.TouchUpInside += OperatorButtonClick;
+            btnSubtract.TouchUpInside += OperatorButtonClick;
+            btnMultiply.TouchUpInside += OperatorButtonClick;
+            btnDivide.TouchUpInside += OperatorButtonClick;
+
+            btnEquals.TouchUpInside += EqualsButtonClick;
+        }
+
+        private void EqualsButtonClick( object sender, EventArgs eventArgs ) {
+            _operand2 = txtDisplay.Text;
+
+            txtDisplay.Text = _calculator.Calculate( _operand1, _operand2, _operator );
+        }
+
+        private void OperatorButtonClick( object sender, EventArgs eventArgs ) {
+            _operand1 = txtDisplay.Text;
+            var button = (UIButton) sender;
+
+            switch ( button.CurrentTitle ) {
+                case "+":
+                    _operator = Operator.Addition;
+                    break;
+                case "-":
+                    _operator = Operator.Subtraction;
+                    break;
+                case "X":
+                    _operator = Operator.Multiplication;
+                    break;
+                case "/":
+                    _operator = Operator.Division;
+                    break;
+            }
+
+            txtDisplay.Text = string.Empty;
+        }
+
+        private void NumericButtonClick( object sender, EventArgs eventArgs ) {
+            var button = (UIButton) sender;
+            txtDisplay.Text += button.CurrentTitle;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ finish/App5/ViewController.designer.cs
@@ -0,0 +1,149 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace App5
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnAdd { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnDivide { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnEight { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnEquals { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnFive { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnFour { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnMultiply { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnNine { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnOne { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnSeven { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnSix { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnSubtract { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnThree { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnTwo { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnZero { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtDisplay { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnAdd != null) {
+				btnAdd.Dispose ();
+				btnAdd = null;
+			}
+			if (btnDivide != null) {
+				btnDivide.Dispose ();
+				btnDivide = null;
+			}
+			if (btnEight != null) {
+				btnEight.Dispose ();
+				btnEight = null;
+			}
+			if (btnEquals != null) {
+				btnEquals.Dispose ();
+				btnEquals = null;
+			}
+			if (btnFive != null) {
+				btnFive.Dispose ();
+				btnFive = null;
+			}
+			if (btnFour != null) {
+				btnFour.Dispose ();
+				btnFour = null;
+			}
+			if (btnMultiply != null) {
+				btnMultiply.Dispose ();
+				btnMultiply = null;
+			}
+			if (btnNine != null) {
+				btnNine.Dispose ();
+				btnNine = null;
+			}
+			if (btnOne != null) {
+				btnOne.Dispose ();
+				btnOne = null;
+			}
+			if (btnSeven != null) {
+				btnSeven.Dispose ();
+				btnSeven = null;
+			}
+			if (btnSix != null) {
+				btnSix.Dispose ();
+				btnSix = null;
+			}
+			if (btnSubtract != null) {
+				btnSubtract.Dispose ();
+				btnSubtract = null;
+			}
+			if (btnThree != null) {
+				btnThree.Dispose ();
+				btnThree = null;
+			}
+			if (btnTwo != null) {
+				btnTwo.Dispose ();
+				btnTwo = null;
+			}
+			if (btnZero != null) {
+				btnZero.Dispose ();
+				btnZero = null;
+			}
+			if (txtDisplay != null) {
+				txtDisplay.Dispose ();
+				txtDisplay = null;
+			}
+		}
+	}
+}
